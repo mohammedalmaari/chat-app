011e42187c1a3911de317b663db89db7916e37e4
mohammedalmaari <145218003+mohammedalmaari@users.noreply.github.com>
2025-02-12 11:25:12 +0300

    update

--- chat-server/index.js
+++ chat-server/index.js
@@ -5,12 +5,15 @@ const { Server, Socket } = require("socket.io");
 const cors = require("cors");
 
 app.use(cors());
-
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    // origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
@@ -28,6 +31,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(5000, () => {
-  console.log("SERVER IS RUNNING");
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`SERVER IS RUNNING ON PORT ${PORT}`);
 });
